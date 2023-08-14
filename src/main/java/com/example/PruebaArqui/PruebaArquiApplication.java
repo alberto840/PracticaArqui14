88fa6901066d2964795ac2707a4245f2876b284d
alberto840 <104152036+alberto840@users.noreply.github.com>
2023-08-14 18:57:47 -0400

    subiendo el pack initializaer

---
+++ src/main/java/com/example/PruebaArqui/PruebaArquiApplication.java
@@ -0,0 +1,13 @@
+package com.example.PruebaArqui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaArquiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaArquiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PruebaArqui/PruebaArquiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PruebaArqui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaArquiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
