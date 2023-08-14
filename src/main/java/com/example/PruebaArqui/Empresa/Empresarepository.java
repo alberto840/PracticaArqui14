ad7799690db46c52f9020c2b3fa45ca19828a31a
alberto840 <104152036+alberto840@users.noreply.github.com>
2023-08-14 19:48:51 -0400

    Corrigiendo Empresarepository.java

--- src/main/java/com/example/PruebaArqui/Empresa/Empresarepository.java
+++ src/main/java/com/example/PruebaArqui/Empresa/Empresarepository.java
@@ -1,9 +1,12 @@
 package com.example.PruebaArqui.Empresa;
 
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface Empresarepository extends JpaRepository<Empresaentity, Integer> {
+    Optional<Empresaentity> findEnterpriseByName(String email);
+
 }
